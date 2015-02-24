8638de3c85429f1b6174c24ef3db119f37e8ec00
jjyim <jjoshyim@gmail.com>
2015-02-24 12:46:03 -0800

    adjusting the sliding pizzas

--- views/js/main.js
+++ views/js/main.js
@@ -530,9 +530,7 @@ document.addEventListener('DOMContentLoaded', function() {
   for (var i = 30; i--;) {
     var elem = document.createElement('img');
     elem.className = 'mover';
-    elem.src = "images/pizza.png";
-    elem.style.height = "100px";
-    elem.style.width = "73.333px";
+    elem.src = "images/sliding-pizza.png";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
