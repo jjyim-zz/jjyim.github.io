8d52026cbce97d92226653bb96f9a24e051fc43b
jjyim <jjoshyim@gmail.com>
2015-02-23 16:40:29 -0800

    change a broken for loop

--- views/js/main.js
+++ views/js/main.js
@@ -450,9 +450,8 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    var pizzaContainerLength = document.querySelectorAll(".randomPizzaContainer").length;
 
-    for (var i = pizzaContainerLength; i-=1) {
+    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
       var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
       var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
       document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
